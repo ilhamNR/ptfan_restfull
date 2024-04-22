07d5ae250d00a9645bcb47ee9812aa0fc023806f
IlhamNR <ilhamnurramadhan19@gmail.com>
2024-04-22 10:24:43 +0700

    add API routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\API\Auth\LoginController;
+use App\Http\Controllers\API\User\PresenceController;
+use App\Http\Controllers\API\Supervisor\ApprovalController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +17,22 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+// Login route
+Route::post('/login', [LoginController::class, 'login']);
 
+// Authenticated routes
+Route::middleware('auth:sanctum')->group(function () {
+    // User presence route
+    Route::post('/presence', [PresenceController::class, 'presence']);
+
+    // Supervisor approval route
+    Route::post('/approve-presence', [ApprovalController::class, 'approvePresence']);
+
+    // Logout route
+    Route::post('/logout', [LoginController::class, 'logout']);
+});
+
+// Default route for retrieving user data
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
